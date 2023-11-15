3f95da1fd9c4ba253ecaf4988123d82983157145
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2023-11-15 19:17:22 -0300

    First working version of the game

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 int main(){
 
     // window demensios
-    int windowWidth{350};
-    int windowHeight{200};
+    int windowWidth{800};
+    int windowHeight{450};
 
     InitWindow(windowWidth,windowHeight,"Marcos' window");
 
     // circles coordinates
-    int circleX{175};
-    int circleY{100};
-    float circleRadius{25};
+    int circleX{windowHeight / 2};
+    int circleY{windowHeight / 2};
+    int circleRadius{25};
+
+    // circle edges
+    int lCircleX{circleX - circleRadius};
+    int rCircleX{circleX + circleRadius};
+    int uCircleY{circleY - circleRadius};
+    int bCircleY{circleY + circleRadius};
+
+    // axe coordinates
+    int axeX{windowWidth / 2};
+    int axeY{0};
+    int axeLength{50};
+
+    // axe edges
+    int lAxeX{axeX};
+    int rAxeX{axeX + axeLength};
+    int uAxeY{axeY};
+    int bAxeY{axeY + axeLength};
+
+    int direction{10};
+
+    bool collisionWithAxe{false};
 
     SetTargetFPS(60);
     
@@ -19,18 +40,51 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // Game logig begins
-        DrawCircle(circleX,circleY,circleRadius,BLUE);
 
-        if(IsKeyDown(KEY_D) && circleX < 350){
-            circleX = circleX + 10;
+        if(collisionWithAxe){
+            DrawText("A collision happned! Game over for you!!",200,200,20,RED);
+            ClearBackground(ORANGE);
+        }else{
+            // Game logig begins
+
+            // Update the edges
+            lCircleX = circleX - circleRadius;
+            rCircleX = circleX + circleRadius;
+            uCircleY = circleY - circleRadius;
+            bCircleY = circleY + circleRadius;
+            lAxeX = axeX;
+            rAxeX = axeX + axeLength;
+            uAxeY = axeY;
+            bAxeY = axeY + axeLength;
+
+            // update colision with axe
+            collisionWithAxe = 
+                    (bAxeY >= uCircleY) && 
+                    (uAxeY <= bCircleY) && 
+                    (lAxeX <= rCircleX) && 
+                    (rAxeX >= lCircleX);
+
+
+            DrawCircle(circleX,circleY,circleRadius,BLUE);
+            DrawRectangle(axeX,axeY,axeLength,axeLength,RED);
+
+            axeY += direction;
+            if (axeY > (windowHeight - axeLength) || axeY < 0 ){
+                direction = -direction;
+            }
+
+            if(IsKeyDown(KEY_D) && circleX < (windowWidth - circleRadius)){
+                circleX += 10;
+            }
+
+            if(IsKeyDown(KEY_A) && circleX > (0 + circleRadius)){
+                circleX -= 10;
+            }
+
+            // Game logic ends
         }
-
-        if(IsKeyDown(KEY_A && circleX > 0)){
-            circleX = circleX - 10;
-        }
-
-        // Game logic ends
+        
         EndDrawing();
+
     }
 }
